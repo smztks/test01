745a226d4a6e7e387eef474eafdc53bd6303381b
unknown <Administrator@.(none)>
2011-11-04 11:30:21 +0900

    First commit

---
+++ src/sample01.scala
@@ -0,0 +1,14 @@
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: 11/11/04
+ * Time: 11:27
+ * To change this template use File | Settings | File Templates.
+ */
+
+object sample01 {
+  def main(args: Array[String]) {
+    println("sample01")
+  }
+}
