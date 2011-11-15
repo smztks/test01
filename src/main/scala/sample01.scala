1f909bfba6683f6a14d2d6bd784e47f452272e7f
unknown <Administrator@.(none)>
2011-11-15 10:53:39 +0900

    14

--- src/main/scala/sample01.scala
+++ src/main/scala/sample01.scala
@@ -18,7 +18,7 @@ class ElementSuite extends FunSuite {
 
 object sample01 {
   def main(args: Array[String]) {
-    println("sample01f")
+    println("sample01i")
 
     val es = new ElementSuite
     es.execute()
