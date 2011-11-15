29f17e8641fbb953266b16bfbeef1d9239a5dd7c
smztks <smztks@undulatus.biz>
2011-11-15 14:37:14 +0900

    21

--- src/main/scala/sample01.scala
+++ src/main/scala/sample01.scala
@@ -18,7 +18,7 @@ class ElementSuite extends FunSuite {
 
 object sample01 {
   def main(args: Array[String]) {
-    println("sample01o")
+    println("sample01p")
 
     val es = new ElementSuite
     es.execute()
